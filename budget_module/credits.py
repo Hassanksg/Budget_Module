2448374f9e8a8b0b148c8550007134179cca3f6a
Hassanksg <91823350+Hassanksg@users.noreply.github.com>
2025-08-26 14:20:39 +0100

    Update credits.py

--- budget_module/credits.py
+++ budget_module/credits.py
@@ -16,6 +16,7 @@ from logging import getLogger
 from pymongo import errors
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from translations import trans
 
 limiter = Limiter(key_func=get_remote_address)
 
